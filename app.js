7f4777f6a5a14e6ef52f9b4f45dffc7e87da12b3
ANDRES GONZALEZ PASCUAL <pascual.andres@inegi.org.mx>
2024-01-23 15:12:10 -0600

    Actividades modulo 2

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-let numeroSecreto = 6;
-let numeroDeUsuario = prompt("¿Me indicas un numero por favor:?");
+//variables
+let numeroSecreto = 3;
+let numeroDeUsuario = 0;
+let intentos = 1;
+let palabraVeces = "vez";
+while (numeroDeUsuario != numeroSecreto) {
+  numeroDeUsuario = prompt("¿Me indicas un numero entre 1 y 10 por favor:?");
 
-console.log(numeroDeUsuario);
+  console.log(numeroDeUsuario);
 
-if (numeroDeUsuario == numeroSecreto) {
-  alert("Adivinaste !!!");
-} else {
-  alert("Lo siento, intenta nuevamente");
+  /*
+    Este codigo realiza la comparacion entre el numero secreto y el numero ingresado por el usuario
+    */
+  if (numeroDeUsuario == numeroSecreto) {
+    alert(
+      `Acertaste, el numero es ${numeroDeUsuario} lo hiciste en ${intentos} ${palabraVeces}`
+    );
+  } else {
+    if (numeroDeUsuario > numeroSecreto) {
+      alert("El numero secreto es menor");
+    } else {
+      alert("El numero secreto es mayor");
+    }
+    intentos++;
+    palabraVeces = "veces";
+    if (intentos > 3) {
+      alert("Llegaste al numero maximo de intentos");
+      break;
+    }
+    //alert("Lo siento, intenta nuevamente");
+  }
 }
