b395162988048a3ba9f863554cb5b04c523aecef
ANDRES GONZALEZ PASCUAL <pascual.andres@inegi.org.mx>
2024-01-23 11:33:49 -0600

    Sentencia If

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-alert("Hola Mundo");
+let numeroSecreto = 6;
 let numeroDeUsuario = prompt("¿Me indicas un numero por favor:?");
 
+console.log(numeroDeUsuario);
 
+if (numeroDeUsuario == numeroSecreto) {
+  alert("Adivinaste !!!");
+} else {
+  alert("Lo siento, intenta nuevamente");
+}
