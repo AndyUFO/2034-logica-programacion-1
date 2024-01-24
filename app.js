bfd6fe439225f5051b52bf31b52d9f35e4d479b2
ANDRES GONZALEZ PASCUAL <pascual.andres@inegi.org.mx>
2024-01-24 15:24:47 -0600

    Fin seccion 2

---
+++ app.js
@@ -0,0 +1,23 @@
+let numeroSecreto = generarNumeroSecreto();
+
+
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+}
+
+function verificarIntento() {
+  let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+  console.log(typeof numeroDeUsuario);
+  console.log(numeroSecreto);
+  console.log(typeof numeroSecreto);
+  console.log(numeroDeUsuario);
+  console.log(numeroDeUsuario===numeroSecreto);
+}
+
+function generarNumeroSecreto() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+asignarTextoElemento("h1", "Juego del numero secreto");
+asignarTextoElemento("p", "Indica un numero del 1 al 10");
